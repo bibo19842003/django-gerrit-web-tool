2550d2556cba075c1bf0e4925aea205188453561
bibo <bibo19842003@gmail.com>
2016-01-01 16:42:41 +0800

    Use bootstrap which version is 3.3.5

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'server',
+    'gerrit',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -82,3 +83,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPLATE_DIRS = (
+  os.path.join(BASE_DIR, 'templates'),
+)
+
+STATICFILES_DIRS = [
+  os.path.join(BASE_DIR, 'static'),
+]
